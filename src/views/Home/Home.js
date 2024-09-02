3e23e3064ec717c7a9160f2dc187cd5311ab3954
kunalsonwane77 <kunalsonwane77@gmail.com>
2024-09-02 18:25:57 +0530

    toasted added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,13 +2,23 @@ import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Homeimg from "./Home-img.png"
 import "./Home.css"
+import toast, { Toaster } from 'react-hot-toast'
 
 function Home() {
   return (
     <div>
       <Navbar/>
       <h1 className='tagbar'>Home</h1>
+      <button className='btntost' type='button' onClick={()=>{
+          toast.success("succesfully Loaded")
+       }}>
+          Load success
+       </button>
       <img src={Homeimg} className='img'/>
+       
+      <div>
+        <Toaster/>
+      </div>
     </div>
   )
 }
